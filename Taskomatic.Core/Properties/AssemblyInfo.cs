32c20a238c0cd66d0ab7b11c7931effae19746c0
Friedrich von Never <friedrich@fornever.me>
2022-12-04 19:28:12 +0100

    Projects: migrate to .NET SDK

--- Taskomatic.Core/Properties/AssemblyInfo.cs
+++ Taskomatic.Core/Properties/AssemblyInfo.cs
@@ -2,11 +2,4 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Taskomatic.Core")]
-[assembly: AssemblyProduct("Taskomatic")]
-[assembly: AssemblyCopyright("Copyright © F. von Never 2017")]
-[assembly: ComVisible(false)]
-[assembly: Guid("3415e384-9f0d-49f6-8742-e79a91c1b7bb")]
-[assembly: AssemblyVersion("0.0.1.0")]
-
 [assembly: InternalsVisibleTo("Taskomatic.Tests")]
--- Taskomatic.Gui/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Taskomatic.Gui")]
-[assembly: AssemblyProduct("Taskomatic")]
-[assembly: AssemblyCopyright("Copyright © F. von Never 2017")]
-[assembly: ComVisible(false)]
-[assembly: Guid("869b83c8-984e-469f-aacf-7eaf5db97d43")]
-[assembly: AssemblyVersion("0.0.1.0")]
